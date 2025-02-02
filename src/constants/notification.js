c5c432e07c015b3dbe05d40e92e37f8e52dfa1a8
Santiago Vigil <santiagovigil288@gmail.com>
2025-02-01 21:18:03 -0300

    [6th commit] change copy

--- src/constants/notification.js
+++ src/constants/notification.js
@@ -1,4 +1,4 @@
 export const ERROR_NOTIFICATION = {
     message: 'Error loading data',
-    description: 'There was an issue fetching the data. Please, adjust your filters and try again.'
+    description: 'There was an issue fetching the data. Please, adjust the filters and try again.'
 }
